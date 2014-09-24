329b67b42409b8d6b754f5b32c9e1f1eb017d468
Homer <lijianbin0719@gravatar.com>
2014-09-24 19:07:11 +0800

    2

--- src/main/java/com/walemao/megastore/controller/UserController.java
+++ src/main/java/com/walemao/megastore/controller/UserController.java
@@ -5,6 +5,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.portlet.bind.annotation.RenderMapping;
 
 @Controller
@@ -13,7 +14,8 @@ public class UserController extends BaseController {
 	
 	@RequestMapping(value={"/users"}, method = { RequestMethod.GET })
 	public String getUsers(){
-		return "";
+		
+		return "index";
 	}
 	
 }
