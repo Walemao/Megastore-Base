e9e6fd0c4c763f8cf271eaf12fd5c11dba829df0
Homer <lijianbin0719@gravatar.com>
2014-10-09 15:06:34 +0800

    提交获取图片列表的方法

--- src/main/java/com/walemao/megastore/repository/ProductImageDao.java
+++ src/main/java/com/walemao/megastore/repository/ProductImageDao.java
@@ -1,9 +1,11 @@
 package com.walemao.megastore.repository;
 
+import java.util.List;
+
 import com.walemao.megastore.domain.ProductImage;
 
 public interface ProductImageDao {
 	public int insert(ProductImage pi);
-
+	public List<ProductImage> getProductImages(int productId);
 	public void update(int productId, int[] ids);
 }
--- src/main/java/com/walemao/megastore/repository/impl/ProductImageDaoImpl.java
+++ src/main/java/com/walemao/megastore/repository/impl/ProductImageDaoImpl.java
@@ -1,10 +1,13 @@
 package com.walemao.megastore.repository.impl;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.walemao.megastore.domain.ProductImage;
+import com.walemao.megastore.domain.mapper.ProductImageMapper;
 import com.walemao.megastore.repository.ProductImageDao;
 import com.walemao.megastore.util.ToolUtil;
 
@@ -31,6 +34,14 @@ public class ProductImageDaoImpl extends CommonDaoImpl implements
 					+ ToolUtil.IntDataToString(ids);
 			this.jdbcTemplate.update(sql, new Object[] { productId });
 		}
+	}
+
+	@Override
+	public List<ProductImage> getProductImages(int productId) {
+		// TODO Auto-generated method stub
+		String sql = "select pi_id,pi_src,pi_md5,pi_sort,pi_productid from t_product_images where pi_productid=? order by pi_sort";
+		return this.jdbcTemplate.query(sql, new Object[] { productId },
+				new ProductImageMapper());
 	}
 
 }
