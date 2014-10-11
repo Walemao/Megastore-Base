f0aae4f25f98fa9d30be1078b6a72d2d307c9858
Homer <lijianbin0719@gravatar.com>
2014-10-11 15:49:42 +0800

    提交代码

--- src/test/java/com/walemao/megastore/InsertUser.java
+++ src/test/java/com/walemao/megastore/InsertUser.java
@@ -1,10 +1,20 @@
 package com.walemao.megastore;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
 public class InsertUser {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		List<String> list = new ArrayList<String>();
+		for (int i = 0; i < 3000; ++i) {
+			String name = UUID.randomUUID().toString();
+			list.add(name);
+			System.out.println("insert into t_user(u_username) values ('" + name + "');");
+		}
+		//t.Insert(list);
 	}
 
 }
--- src/test/java/com/walemao/megastore/Test.java
+++ src/test/java/com/walemao/megastore/Test.java
@@ -1,5 +1,39 @@
 package com.walemao.megastore;
 
-public class Test {
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.List;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.jdbc.core.BatchPreparedStatementSetter;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+public class Test {
+	ApplicationContext ac = new FileSystemXmlApplicationContext("/WEB-INF/spring/spring-context.xml");
+	JdbcTemplate jdbcTemplate = (JdbcTemplate) ac.getBean("jdbcTemplate");
+
+	public List<String> Insert(final List<String> mList) {
+		System.out.println("#########################");
+		String sql0 = "select 1 from t_user where u_username='abc' limit 1";
+		int key = this.jdbcTemplate.update(sql0);
+		System.out.println(key);
+		String sql = "insert into t_user(u_username) values(?)";
+		jdbcTemplate.batchUpdate(sql, new BatchPreparedStatementSetter() {
+			@Override
+			public void setValues(PreparedStatement ps, int i)
+					throws SQLException {
+				// TODO Auto-generated method stub
+				String name = mList.get(i);
+				ps.setString(1, name);
+			}
+
+			@Override
+			public int getBatchSize() {
+				// TODO Auto-generated method stub
+				return mList.size();
+			}
+		});
+		return mList;
+	}
 }
