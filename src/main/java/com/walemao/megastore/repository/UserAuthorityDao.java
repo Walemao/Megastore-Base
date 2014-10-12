9ece911a2b3a4f25c3ea70be86631525c7cb84c7
Homer <lijianbin0719@gravatar.com>
2014-10-12 13:37:34 +0800

    提交用户权限daoimpl实现类

--- src/main/java/com/walemao/megastore/repository/UserAuthorityDao.java
+++ src/main/java/com/walemao/megastore/repository/UserAuthorityDao.java
@@ -3,7 +3,19 @@ package com.walemao.megastore.repository;
 import com.walemao.megastore.domain.UserAuthority;
 
 public interface UserAuthorityDao {
+	/**
+	 * 插入权限方法
+	 * @param au
+	 */
 	public void insert(UserAuthority au);
+	/**
+	 * 修改权限方法
+	 * @param au 传入修改权限的对象和用户名
+	 */
 	public void update(UserAuthority au);
+	/**
+	 * 删除用户权限
+	 * @param username
+	 */
 	public void delete(String username);
 }
--- src/main/java/com/walemao/megastore/repository/impl/UserAuthorityDaoImpl.java
+++ src/main/java/com/walemao/megastore/repository/impl/UserAuthorityDaoImpl.java
@@ -1,26 +1,40 @@
 package com.walemao.megastore.repository.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
 import com.walemao.megastore.domain.UserAuthority;
 import com.walemao.megastore.repository.UserAuthorityDao;
 
+@Repository
 public class UserAuthorityDaoImpl implements UserAuthorityDao {
+
+	@Autowired
+	private JdbcTemplate JdbcTemplate;
 
 	@Override
 	public void insert(UserAuthority au) {
 		// TODO Auto-generated method stub
-
+		String sql = "insert into t_user_authority(username,authority) values(?,?)";
+		this.JdbcTemplate.update(sql,
+				new Object[] { au.getUsername(), au.getAuthority() });
 	}
 
 	@Override
 	public void update(UserAuthority au) {
 		// TODO Auto-generated method stub
-
+		String sql = "update t_user_authority set authority=? where username=?";
+		this.JdbcTemplate.update(sql,
+				new Object[] { au.getAuthority(), au.getUsername() });
 	}
 
 	@Override
 	public void delete(String username) {
 		// TODO Auto-generated method stub
-
+		String sql = "delete from t_user_authority where username=?";
+		this.JdbcTemplate.update(sql,
+				new Object[] { username });
 	}
 
 }
