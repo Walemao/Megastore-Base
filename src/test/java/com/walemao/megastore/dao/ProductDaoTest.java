9b596698902bac87257ec3a879df7b87c8baff8d
tyjuncai <tyjuncai@qq.com>
2014-10-13 22:18:58 +0800

    单元测试例子
    单元测试例子

--- src/test/java/com/walemao/megastore/dao/ProductDaoTest.java
+++ src/test/java/com/walemao/megastore/dao/ProductDaoTest.java
@@ -1,6 +1,10 @@
 package com.walemao.megastore.dao;
 
+import java.sql.Timestamp;
+import java.util.Date;
+
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +14,7 @@ import org.springframework.test.context.junit4.AbstractTransactionalJUnit4Spring
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.transaction.TransactionConfiguration;
 
+import com.walemao.megastore.domain.ProductBase;
 import com.walemao.megastore.repository.ProductBaseDao;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -20,29 +25,39 @@ public class ProductDaoTest extends
 	
 	@Before
 	public void testBefore() {
+		this.executeSqlScript("/com/walemao/megastore/sql/add_product_test_data.sql", false);
 	}
 
 	@After
 	public void testAfter() {
+		this.executeSqlScript("/com/walemao/megastore/sql/delete_product_test_data.sql", false);
 	}
 
 	@Autowired
 	private ProductBaseDao productBaseDao;
 
 	/****** 以下开始编写测试用例 *******/
-	@Test
-	public void t_00_findProducts() {
-
-	}
-
-	@Test
-	public void t_01_findProductById() {
-
-	}
 
 	@Test
 	public void t_02_addProduct() {
-
+		ProductBase pb = new ProductBase();
+		pb.setName("飞机模型");
+		int i = productBaseDao.insert(pb);
+		ProductBase pbDB =  productBaseDao.getProduct(i);
+		
+		//预测结果和实际结果对比
+		Assert.assertEquals("飞机模型", pbDB.getName());
+	}
+	
+	@Test
+	public void t_01_findProductById() {
+		ProductBase pb = new ProductBase();
+		pb.setName("飞机模型");
+		int i = productBaseDao.insert(pb);
+		ProductBase pbDB =  productBaseDao.getProduct(i);
+		
+		//预测结果和实际结果对比
+		Assert.assertEquals("飞机模型", pbDB.getName());
 	}
 
 }
---
+++ src/test/resources/com/walemao/megastore/sql/add_product_test_data.sql
@@ -0,0 +1,1 @@
+insert into `t_product_base`(`p_name`,`p_number`,`p_classify`,`p_desc`,`p_recommend`) values ('电动玩具', 'ADKD-JK43', 1, '这是一个电动玩具', 1);
