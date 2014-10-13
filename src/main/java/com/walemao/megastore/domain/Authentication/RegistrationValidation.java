3288ba579ae3b5ecddd638b08a4a12eeaf0fb260
ylemzy <devildomelf@163.com>
2014-10-13 20:22:38 +0800

    临时解决validator null pointer

--- src/main/java/com/walemao/megastore/domain/Authentication/RegistrationValidation.java
+++
@@ -1,8 +0,0 @@
-package com.walemao.megastore.domain.Authentication;
-
-import org.springframework.validation.Errors;
-
-public interface RegistrationValidation {
-
-	public void validate(Object target, Errors errors);
-}
--- src/main/java/com/walemao/megastore/domain/validation/RegistrationValidator.java
+++ src/main/java/com/walemao/megastore/domain/validation/RegistrationValidator.java
@@ -1,26 +1,33 @@
-package com.walemao.megastore.domain.Authentication;
+package com.walemao.megastore.domain.validation;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
 
 import com.walemao.megastore.domain.User;
+import com.walemao.megastore.repository.UserDao;
 
 
-public class RegistrationValidationImpl implements RegistrationValidation
+public class RegistrationValidator implements Validator
 {
-	private Logger logger = LoggerFactory.getLogger(RegistrationValidation.class);
+	private Logger logger = LoggerFactory.getLogger(RegistrationValidator.class);
+	
+	@Autowired
+	private UserDao userDao;
+	
 	public boolean supports(Class<?> klass) 
 	{
 		return User.class.isAssignableFrom(klass);
 	}
-
+	
 	public void validate(Object target, Errors errors) 
 	{
 		User user = (User) target;
-/*		ValidationUtils.rejectIfEmptyOrWhitespace(user.getUsername(), "userName",
+		/*ValidationUtils.rejectIfEmptyOrWhitespace(errors, "userName",
 				"NotEmpty.registration.userName",
 				"User Name must not be Empty.");*/
 
@@ -32,6 +39,14 @@ public class RegistrationValidationImpl implements RegistrationValidation
 					"lengthOfUser.registration.userName",
 					"User Name must not more than 50 characters.");
 		}
+		
+		if (true || userDao.CheckUsername(user.getUsername()))
+		{
+			errors.rejectValue("userName",
+					"User:userName",
+					"User Name exists");
+		}
+		
 		
 		if (!(user.getPassword()).equals(user.getConfirmPassword())) 
 		{
